0e70520143aad4cf868f6a07bdfcb99b482393f4
shubhswain45 <shubhkantswain@.com>
2025-01-05 18:33:34 +0530

    some fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,8 @@
 import LikedTracksHeader from '@/components/_home/LikedTracksHeader';
 import SectionGrid from '@/components/_home/SectionGrid';
-import SectionCard from '@/components/_home/SectionCard';
 import Footer from '@/components/_home/Footer';
 
-interface PlaylistCard {
-  id: number;
-  title: string;
-  description: string;
-  imageUrl?: string;
-}
-
-// Mock data
-const makeForYouItems: PlaylistCard[] = Array.from({ length: 10 }, (_, i) => ({
-  id: i,
-  title: `Daily Mix ${i + 1}`,
-  description: 'Custom playlist for you'
-}));
-
-const recentlyPlayedItems: PlaylistCard[] = Array.from({ length: 10 }, (_, i) => ({
-  id: i,
-  title: `Album Title ${i + 1}`,
-  description: 'Artist Name'
-}));
-
-const topMixesItems: PlaylistCard[] = Array.from({ length: 5 }, (_, i) => ({
-  id: i,
-  title: `Mix ${i + 1}`,
-  description: 'Based on your listening'
-}));
-
 const HomePage: React.FC = () => {
-
-
   return (
     <div className="space-y-6">
       {/* Liked Track Header */}
