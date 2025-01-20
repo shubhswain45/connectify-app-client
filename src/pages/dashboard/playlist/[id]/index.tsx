059f51e0725ad2cf4c81da427034df867f07bf97
shubhswain45 <shubhkantswain@.com>
2025-01-20 15:40:00 +0530

    y

--- src/pages/dashboard/playlist/[id]/index.tsx
+++ src/pages/dashboard/playlist/[id]/index.tsx
@@ -112,7 +112,7 @@ const SpotifyPlaylist = () => {
         {songs.map((song) => (
           <div
             key={song.id}
-            className="flex items-center gap-3 p-3 bg-transparent hover:bg-gray-800/50 hover:scale-105 hover:shadow-md transition-all duration-300 ease-out rounded-lg"
+            className="flex items-center gap-3 p-3 bg-hover-animate rounded-lg"
           >
             <img
               src="https://cdn.vox-cdn.com/thumbor/J1CL5bMzlkZAwj9vEdFtqsAewTA=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/25564546/sethrollins.jpg"
@@ -129,6 +129,10 @@ const SpotifyPlaylist = () => {
           </div>
         ))}
       </div>
+
+
+
+
 
 
     </div>
